c7437f82b03b54b3351b97ba106583bdb0784273
Pawel Englert <pawele@splunk.com>
2024-10-13 10:19:59 +0200

    Add terraform

---
+++ terraform/sample/custom_names_example/main.tf
@@ -0,0 +1,23 @@
+module "example_aws_pool_1" {
+  source = "../../workload-identity-pool"
+
+  project_id             = "my-project-id"
+  realm_name             = "us0"
+  additional_project_ids = ["additional1", "additional2"]
+  custom_pool_name       = "custom-pool-name"
+  custom_provider_name   = "custom-provider-name"
+}
+
+resource "signalfx_gcp_integration" "integration1" {
+  name         = "integration1"
+  enabled      = true
+  poll_rate    = 600
+  services     = ["compute"]
+  include_list = ["labels"]
+  auth_method  = "WORKLOAD_IDENTITY_FEDERATION"
+  project_wif_configs {
+    project_id = "my-project-id"
+    wif_config = module.example_aws_pool_1.credentials_file_content
+
+  }
+}
---
+++ terraform/sample/multi_account_example/main.tf
@@ -0,0 +1,48 @@
+module "example_aws_pool_1" {
+  source = "../../workload-identity-pool"
+
+  project_id             = "my-project-id"
+  realm_name             = "us0"
+  additional_project_ids = ["additional1", "additional2"]
+}
+
+
+resource "signalfx_gcp_integration" "integration1" {
+  name         = "integration1"
+  enabled      = true
+  poll_rate    = 600
+  services     = ["compute"]
+  include_list = ["labels"]
+  auth_method  = "WORKLOAD_IDENTITY_FEDERATION"
+  project_wif_configs {
+    project_id = "my-project-id"
+    wif_config = module.example_aws_pool_1.credentials_file_content
+
+  }
+}
+
+resource "signalfx_gcp_integration" "integration2" {
+  name         = "integration2"
+  enabled      = true
+  poll_rate    = 600
+  services     = ["compute"]
+  include_list = ["labels"]
+  auth_method  = "WORKLOAD_IDENTITY_FEDERATION"
+  project_wif_configs {
+    project_id = "additional1"
+    wif_config = module.example_aws_pool_1.credentials_file_content
+  }
+}
+
+resource "signalfx_gcp_integration" "integration3" {
+  name         = "integration3"
+  enabled      = true
+  poll_rate    = 600
+  services     = ["compute"]
+  include_list = ["labels"]
+  auth_method  = "WORKLOAD_IDENTITY_FEDERATION"
+  project_wif_configs {
+    project_id = "additional2"
+    wif_config = module.example_aws_pool_1.credentials_file_content
+  }
+}
